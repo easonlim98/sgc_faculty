b871e3b71fd3adaba92a1c261e130045f83d79a9
easonlim <easonlim98@gmail.com>
2022-03-15 22:46:21 +0800

    changes

--- src/components/Foundation.js
+++ src/components/Foundation.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './css/foundation.css'
 import 'bootstrap/dist/css/bootstrap.css';
-import './css/standard.css'
+import './css/Standard.css'
 
 const Foundation = () => {
   return (
