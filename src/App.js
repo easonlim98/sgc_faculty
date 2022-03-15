36991011f71e5a0af544bd00837072f63ad0150c
Choong Zhen Yep <zhenyep1998@gmail.com>
2022-03-15 23:18:06 +0800

    zhenyap changes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import Footer from './Footer';
 import LandingScreen from './components/LandingScreen';
 import EarlyChildhoodEducation from './components/EarlyChildhoodEducation';
 import 'bootstrap/dist/css/bootstrap.css';
-<<<<<<< HEAD
-import Admin_Login from './components/Admin_Login';
 import Hospitality from './components/Hospitality';
-=======
 import AdminLogin from './components/AdminLogin';
 import { getDataEvent } from './util/commonDB';
 
@@ -17,7 +14,6 @@ const App = () => {
   useEffect(() => {
     getDataEvent();
   },[]);
->>>>>>> 3ef227251401f35b2c0b481506ddf8a24545eff2
 
   return (
       <Router>
@@ -25,16 +21,10 @@ const App = () => {
         <Header/>
           <div>
           <Routes>
-<<<<<<< HEAD
-            <Route path="/" element={<Landing_Screen/>} />
-            <Route exact path="/Early_Childhood_Education" element={<Early_Childhood_Education/>} />
-            <Route exact path="/Admin_Login" element={<Admin_Login/>} />
-            <Route exact path="/Hospitality" element={<Hospitality/>} />
-=======
-            <Route exact path="/" element={<LandingScreen/>} />
+            <Route path="/" element={<LandingScreen/>} />
             <Route exact path="/EarlyChildhoodEducation" element={<EarlyChildhoodEducation/>} />
             <Route exact path="/AdminLogin" element={<AdminLogin/>} />
->>>>>>> 3ef227251401f35b2c0b481506ddf8a24545eff2
+            <Route exact path="/Hospitality" element={<Hospitality/>} />
           </Routes>
           </div>
           {/* { window.location.pathname !== "/AdminLogin" ? <Footer/> : <></>} */}
--- src/components/Hospitality.js
+++ src/components/Hospitality.js
@@ -7,14 +7,14 @@ import './css/standard.css'
 const Hospitality = () => {
     return (
         <div>
-            <div class="hospitalitybanner">
+            <div className="hospitalitybanner">
 
 
                 <div>
 
-                    <img class="banner" src={require("../assets/images/Hospitality/Introback.png")} alt="" />
+                    <img className="banner" src={require("../assets/images/Hospitality/Introback.png")} alt="" />
 
-                    <div class="set1">
+                    <div className="set1">
 
                         <h2>
                             Know more about
@@ -28,7 +28,7 @@ const Hospitality = () => {
                         </p>
 
 
-                        <button type="button" className="General-Button">Programme</button>
+                        <button type="button" classNameName="General-Button">Programme</button>
                     </div>
 
                 </div>
@@ -40,26 +40,26 @@ const Hospitality = () => {
             <br />
             <br />
 
-            <div class="container">
-                <div class="row sp1">
-                    <div class="col imgborder">
-                        <div class="iconimg">
+            <div className="container">
+                <div className="row sp1">
+                    <div className="col imgborder">
+                        <div className="iconimg">
                             <img src={require("../assets/images/Hospitality/icon1.png")} style={{ width: '100px', height: '100px' }} />
 
                             <p>Involves food and beverage business that can range from restaurants.</p>
                         </div>
                     </div>
 
-                    <div class="col imgborder">
-                        <div class="iconimg">
+                    <div className="col imgborder">
+                        <div className="iconimg">
                             <img src={require("../assets/images/Hospitality/icon2.png")} style={{ width: '100px', height: '100px' }} />
 
                             <p>Operating accommodation establishments</p><br></br>
                         </div>
                     </div>
 
-                    <div class="col imgborder">
-                        <div class="iconimg">
+                    <div className="col imgborder">
+                        <div className="iconimg">
                             <img src={require("../assets/images/Hospitality/icon3.png")} style={{ width: '100px', height: '100px' }} />
 
                             <p>Involves food and beverage business that can range from restaurants.</p>
@@ -73,16 +73,16 @@ const Hospitality = () => {
                 <h2>
                     <b>Hospitality in SEGi</b>
                 </h2>
-                <div class="row">
-                    <div class="col">
-                        <p class="hospitalityp1">The Hospitality & Tourism programmes in SEGi are designed to equip you
+                <div className="row">
+                    <div className="col">
+                        <p className="hospitalityp1">The Hospitality & Tourism programmes in SEGi are designed to equip you
                             with skills and knowledge that
                             are increasingly in demand and relevant through modules that cover marketing, destination
                             management,
                             human resources, information management, planning and research, among others.
                         </p>
                     </div>
-                    <div class="col">
+                    <div className="col">
                         <img src={require("../assets/images/Hospitality/Gif2.gif")} style={{ width: '100%', height: '100%' }} alt="" />
 
                     </div>
@@ -101,7 +101,7 @@ const Hospitality = () => {
                 </p>
 
 
-                <img class="ehancesskillsimg" src={require("../assets/images/Hospitality/EnhanceSkills.JPG")} aboutlt="" />
+                <img className="ehancesskillsimg" src={require("../assets/images/Hospitality/EnhanceSkills.JPG")} aboutlt="" />
 
                 <br />
                 <br />
@@ -115,7 +115,7 @@ const Hospitality = () => {
                     <br></br>
 
                     <li>
-                        There is a need for educated and trained individuals who can deliver first class services.
+                        There is a need for educated and trained individuals who can deliver first className services.
                     </li>
                 </ul>
 
@@ -124,56 +124,56 @@ const Hospitality = () => {
 
             </div>
 
-            <div class="partnershipshotel">
-                <div class="container">
-                    <h2 class="hotelstyleheader">
+            <div className="partnershipshotel">
+                <div className="container">
+                    <h2 className="hotelstyleheader">
                         <b>Partnerships hotel</b>
                     </h2>
 
 
-                    <div class="row">
-                        <div class="col partner1">
+                    <div className="row">
+                        <div className="col partner1">
 
-                            <img class="partnerhotel" alt="Hatten Hotel" src={require("../assets/images/Hospitality/HattenHotel.png")} />
+                            <img className="partnerhotel" alt="Hatten Hotel" src={require("../assets/images/Hospitality/HattenHotel.png")} />
 
-                            <p class="hoteltxt"><b>Hatten Hotel</b></p>
-
-                        </div>
-
-                        <div class="col partner1">
-                            <img class="partnerhotel" src={require("../assets/images/Hospitality/PangkorResort.png")} alt="" />
-
-                            <p class="hoteltxt"><b>Pangkor Resort</b></p>
+                            <p className="hoteltxt"><b>Hatten Hotel</b></p>
 
                         </div>
 
-                        <div class="col partner1">
-                            <img class="partnerhotel" src={require("../assets/images/Hospitality/CopthorneHotel.png")} alt="" />
+                        <div className="col partner1">
+                            <img className="partnerhotel" src={require("../assets/images/Hospitality/PangkorResort.png")} alt="" />
 
-                            <p class="hoteltxt"><b>Copthorne Hotel</b></p>
-
-                        </div>
-
-                        <div class="col partner1">
-                            <img class="partnerhotel" src={require("../assets/images/Hospitality/HardRockHotel.png")} alt="" />
-
-                            <p class="hoteltxt"><b>HardRock Hotel</b></p>
+                            <p className="hoteltxt"><b>Pangkor Resort</b></p>
 
                         </div>
 
-                        <div class="col partner1">
-                            <img class="partnerhotel" src={require("../assets/images/Hospitality/SunwayResort.png")} alt="" />
+                        <div className="col partner1">
+                            <img className="partnerhotel" src={require("../assets/images/Hospitality/CopthorneHotel.png")} alt="" />
 
-                            <p class="hoteltxt"><b>Sunway Resort</b></p>
+                            <p className="hoteltxt"><b>Copthorne Hotel</b></p>
+
+                        </div>
+
+                        <div className="col partner1">
+                            <img className="partnerhotel" src={require("../assets/images/Hospitality/HardRockHotel.png")} alt="" />
+
+                            <p className="hoteltxt"><b>HardRock Hotel</b></p>
+
+                        </div>
+
+                        <div className="col partner1">
+                            <img className="partnerhotel" src={require("../assets/images/Hospitality/SunwayResort.png")} alt="" />
+
+                            <p className="hoteltxt"><b>Sunway Resort</b></p>
 
                         </div>
 
                     </div>
 
 
-                    <div class="row">
-                        <div class="col">
-                            <h2 class="hotelstyleheader">
+                    <div className="row">
+                        <div className="col">
+                            <h2 className="hotelstyleheader">
                                 <b>Hotel Style</b>
                             </h2>
 
@@ -188,20 +188,20 @@ const Hospitality = () => {
                             </ul>
 
                         </div>
-                        <div class="col hotelstyleimage">
-                            <img src={require("../assets/images/Hospitality/Hotelstyle1.png")} class="backimg" />
-                            <img src={require("../assets/images/Hospitality/Hotelstyle2.png")} class="frontimg" />
+                        <div className="col hotelstyleimage">
+                            <img src={require("../assets/images/Hospitality/Hotelstyle1.png")} className="backimg" />
+                            <img src={require("../assets/images/Hospitality/Hotelstyle2.png")} className="frontimg" />
                         </div>
                     </div>
                 </div>
 
-                <div className="Faculties">
-                    <div className="row">
-                        <div className="col-sm-5">
-                            <img className="Faculties-img" src={require("../assets/images/Hospitality/btmimg.png")} alt="" />
+                <div classNameName="Faculties">
+                    <div classNameName="row">
+                        <div classNameName="col-sm-5">
+                            <img classNameName="Faculties-img" src={require("../assets/images/Hospitality/btmimg.png")} alt="" />
                         </div>
-                        <div className="col-sm-7">
-                            <div className="Faculties-right">
+                        <div classNameName="col-sm-7">
+                            <div classNameName="Faculties-right">
                                 <br />
                                 <br />
                                 <p style={{ fontWeight: '700' }}>Want to Know more about our  facilities</p>
@@ -210,7 +210,7 @@ const Hospitality = () => {
                                 <br />
                                 <br />
                                 <div style={{ display: 'flex', justifyContent: 'flex-end', marginBottom: '1rem', marginRight: '2rem' }}>
-                                    <button type="button" className="General-Button">Facilities</button>
+                                    <button type="button" classNameName="General-Button">Facilities</button>
                                 </div>
                             </div>
                         </div>
