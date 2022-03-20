8fc707d8a9bbff132cdcb358b6fb49cf400c3e73
Choong Zhen Yep <zhenyep1998@gmail.com>
2022-03-20 21:15:01 +0800

    Choong changed

--- src/components/AmericanDegree.js
+++ src/components/AmericanDegree.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './css/american_degree.css'
 import 'bootstrap/dist/css/bootstrap.css';
-import './css/Standard.css'
+import './css/standard.css'
 
 const AmericanDegree = () => {
   return (
