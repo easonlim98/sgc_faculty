fd55037549001395f8e6641df51c6398cfe5e014
easonlim <easonlim98@gmail.com>
2022-04-28 04:02:00 +0800

    a

--- controller/getAdminCourse.php
+++ controller/getAdminCourse.php
@@ -5,14 +5,9 @@ header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header("Access-Control-Allow-Headers: *");
 
 	$allCourse = mysqli_query($db,
-        "SELECT F.FacultyName, C.CourseID, C.CourseTitle, C.CourseLevelOfStudy, CL.CollegeName, C.CourseCode, CL.CollegeID
-         FROM available_subject A_S
-         JOIN college CL
-         ON A_S.CollegeID = CL.CollegeID
-		 JOIN faculty F
-		 ON A_S.FacultyID = F.FacultyID
-		 JOIN course C
-		 ON A_S.CourseID = C.CourseID"
+        "SELECT C.CourseID, C.CollegeID, C.FacultyID, C.CourseTitle,
+                C.CourseLevelOfStudy
+         FROM course C"
         );
 		$rows = array();
 		while ($r = mysqli_fetch_assoc($allCourse)) {
